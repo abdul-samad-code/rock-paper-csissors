8b6858992c5f7a53d31d3010a632a0cd2336fdcd
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-03-05 18:22:03 +0000

    Finish Rock Paper Scissors project with 5 rounds logic

---
+++ script.js
@@ -0,0 +1,66 @@
+console.log("hello world");
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3)
+
+    if(randomNumber === 0){
+        return "rock"
+    } 
+    else if(randomNumber === 1){
+        return "paper"
+    } 
+    else{
+        return "scissors"
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice : rock, paper, scissors")
+    return choice
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice === computerChoice){
+        console.log("The game is Tie");
+    } 
+    else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ){
+        humanScore++;
+        console.log("You Win " + humanChoice + " beats " + computerChoice)
+    } 
+    else{
+        computerScore++;
+        console.log("Computer Win " + computerChoice + " beats " + humanChoice)
+    }
+
+    console.log("Your Score: " + humanScore + " Computer Score: " + computerScore)
+}
+
+for(let i = 1; i <= 5; i++){
+    console.log("Round " + i)
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+}
+
+
+if(humanScore > computerScore){
+    console.log("You won the game!");
+} 
+else if(computerScore > humanScore){
+    console.log("Computer won the game!");
+} 
+else{
+    console.log("The game is a tie!");
+}

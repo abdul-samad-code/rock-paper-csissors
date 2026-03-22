fb3ba50f85781015e90a0d761dd4cb6b6ad65732
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-03-22 16:43:22 +0000

    completed ui for rock paper scissors

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 console.log("hello world");
 
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*3)
 
-    if(randomNumber === 0){
-        return "rock"
-    } 
-    else if(randomNumber === 1){
-        return "paper"
-    } 
-    else{
-        return "scissors"
-    }
-}
 
-function getHumanChoice(){
-    let choice = prompt("Enter your choice : rock, paper, scissors")
-    return choice
-}
+
 
 let computerScore = 0;
 let humanScore = 0;
 
-function playRound(humanChoice, computerChoice){
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3)
+    if(randomNumber === 0){
+        return "rock"
+    } else if(randomNumber === 1){
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
 
+function playRound(humanChoice){
+    const computerChoice = getComputerChoice();
     humanChoice = humanChoice.toLowerCase();
-
+    let resultText = "";
     if(humanChoice === computerChoice){
-        console.log("The game is Tie");
+        resultText = "The game is Tie"
     } 
     else if(
         (humanChoice === "rock" && computerChoice === "scissors") ||
@@ -35,32 +31,42 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "scissors" && computerChoice === "paper")
     ){
         humanScore++;
-        console.log("You Win " + humanChoice + " beats " + computerChoice)
+        resultText = "You Win " + humanChoice + " beats " + computerChoice;
     } 
     else{
         computerScore++;
-        console.log("Computer Win " + computerChoice + " beats " + humanChoice)
+        resultText = "Computer Win " + computerChoice + " beats " + humanChoice;
     }
-
-    console.log("Your Score: " + humanScore + " Computer Score: " + computerScore)
+    // shpw result in ui
+    document.getElementById("result").textContent = resultText;
+    document.getElementById("score").textContent = "Your Score: " + humanScore + " Computer score " + computerScore;
+     // winner check
+     if(humanScore === 5){
+        alert("you wom the game");
+        resetGame();
+     } else if (computerScore === 5 ){
+        alert("Computer won the game")
+        resetGame();
+     }
 }
 
-for(let i = 1; i <= 5; i++){
-    console.log("Round " + i)
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
+function resetGame(){
+    humanScore = 0
+    computerScore = 0
+    document.getElementById("score").textContent="Your Score: 0 | Computer Score: 0 "
 }
 
+document.getElementById("rock").addEventListener("click", function(){
+    playRound("rock");
+})
+document.getElementById("paper").addEventListener("click", function(){
+    playRound("paper");
+})
+document.getElementById("scissors").addEventListener("click", function(){
+    playRound("scissors");
+})
 
-if(humanScore > computerScore){
-    console.log("You won the game!");
-} 
-else if(computerScore > humanScore){
-    console.log("Computer won the game!");
-} 
-else{
-    console.log("The game is a tai!");
-}
+
+
+
+
